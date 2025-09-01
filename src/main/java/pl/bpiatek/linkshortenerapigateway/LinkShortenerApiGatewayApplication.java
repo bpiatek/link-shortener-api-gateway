c532c81740078058b089be09a8056c8a3cf37e69
Bartosz Piatek <your.email@example.com>
2025-09-01 13:36:40 +0200

    first commit

---
+++ src/main/java/pl/bpiatek/linkshortenerapigateway/LinkShortenerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenerapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bpiatek/linkshortenerapigateway/LinkShortenerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiatek.linkshortenerapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
