ea5747f3bc69d715967a28d64741998f6345b709
Bartosz Piatek <your.email@example.com>
2025-09-21 07:49:40 +0200

    added security for redirecting links

--- src/main/java/pl/bpiatek/linkshortenerapigateway/SecurityConfig.java
+++ src/main/java/pl/bpiatek/linkshortenerapigateway/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -12,15 +13,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
 
 @EnableConfigurationProperties(MonitoringUserProperties.class)
 @Configuration
 class SecurityConfig {
 
     @Bean
-    public UserDetailsService inMemoryUserDetailsManager(
+    UserDetailsService inMemoryUserDetailsManager(
             MonitoringUserProperties properties,
             PasswordEncoder passwordEncoder
     ) {
@@ -33,22 +38,27 @@ class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     @Order(2)
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(regexMatcher(GET, "/[a-zA-Z0-9]{1,7}"))
+                        .permitAll()
+
+                        .requestMatchers(POST, "/links")
+                        .authenticated()
+
                         .requestMatchers(
                                 "/users/auth/register",
                                 "/users/auth/login",
                                 "/users/auth/refresh",
-                                "/users/.well-known/jwks.json",
-                                "/links"
+                                "/users/.well-known/jwks.json"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
